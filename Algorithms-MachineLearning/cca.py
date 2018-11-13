7711a3fbcce751fb6e513df94a8d00c44f61a463
kdjnolan <43641317+kdjnolan@users.noreply.github.com>
2018-11-13 03:43:33 +0000

    Create cca.py

---
+++ Algorithms-MachineLearning/cca.py
@@ -0,0 +1,25 @@
+
+from skimage import measure
+from skimage.measure import regionprops
+import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import licencePlateDetection
+
+# this gets all the connected regions and groups them together
+label_image = measure.label(licencePlateDetection.binary_car_image)
+fig, (ax1) = plt.subplots(1)
+ax1.imshow(licencePlateDetection.gray_car_image, cmap="gray");
+
+# regionprops creates a list of properties of all the labelled regions
+for region in regionprops(label_image):
+    if region.area < 50:
+        #if the region is so small then it's likely not a license plate
+        continue
+
+    # the bounding box coordinates
+    minRow, minCol, maxRow, maxCol = region.bbox
+    rectBorder = patches.Rectangle((minCol, minRow), maxCol-minCol, maxRow-minRow, edgecolor="red", linewidth=2, fill=False)
+    ax1.add_patch(rectBorder)
+    # let's draw a red rectangle over those regions
+
+plt.show()
