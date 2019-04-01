e7d699074b9e4218f9ed34bd2a4c19c84d790979
kdjnolan <43641317+kdjnolan@users.noreply.github.com>
2019-04-01 19:06:28 +0100

    Update signIn.js

--- FrontEndGUI/SignInApp/static/js/signIn.js
+++ FrontEndGUI/SignInApp/static/js/signIn.js
@@ -1,12 +1,12 @@
 $(function(){
         $('#btnSignIn').click(function(){
-                console.log("Button(btn- signIn from HTML) activated the javascript code");
+                console.log("Button pressed. 'signin.js' now running. Attempting AJAX POST on javascript file");
                 $.ajax({
                         url: '/signIn',
                         data: $('form').serialize(),
                         type: 'POST',
                         success: function(response){
-                                console.log(response);
+                                //console.log(response);                                
                         },
                         error: function(error){
                                 console.log(error);
